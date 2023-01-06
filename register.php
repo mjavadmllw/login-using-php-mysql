6b1ba3c6d1cb30a61216ee919b08a619c2a9212c
M.javad_mllw <mohammadjavadbagheri91011@gmail.com>
2023-01-06 23:27:43 +0330

    updated register.php

--- register.php
+++ register.php
@@ -7,14 +7,18 @@
             if(duplicateChecker($_POST['email'])){
                 $error="Email has been registered before<br>Please use another email";
             }else{
-                registration($_POST['username'],$_POST['password'],$_POST['email']);
-                header("location: login.php");
+                if (preg_match('/[^A-Za-z0-9]/', $_POST['username'])){
+                    $error="Username must only have letters";
+                } else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $error="please enter a valid email";
+                }else{
+                    registration($_POST['username'],$_POST['password'],$_POST['email']);
+                    header("location: login.php");
+                }
             }
         }else{
             $error="Please enter all fileds";
         }
     }
-
-
 
     include __DIR__ . '/views/register.view.php';
